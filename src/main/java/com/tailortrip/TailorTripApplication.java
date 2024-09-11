1d31e90c46ac9bfdbce3d40044d9afb8b58b423e
myo22 <dlalsgud12@naver.com>
2024-09-11 17:35:56 +0900

    Initial commit

---
+++ src/main/java/com/tailortrip/TailorTripApplication.java
@@ -0,0 +1,13 @@
+package com.tailortrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailorTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TailorTripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tailortrip/TailorTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tailortrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailorTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

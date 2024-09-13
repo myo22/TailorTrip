60542e4601c19e813796005989f0d045812a0b2c
myo22 <dlalsgud12@naver.com>
2024-09-13 16:11:47 +0900

    Swagger UI 설정

---
+++ src/main/java/com/tailorTrip/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.tailorTrip.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+// @RestController 어노테이션이 있는 컨트롤러들에 대해서 API문서를 생성하도록 작성합니다.
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)) // 이 설정은 특정 애너테이션이 붙은 클래스의 메서드만 문서화 대상으로 지정합니다.
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("tailorTrip Swagger")
+                .build();
+    }
+}

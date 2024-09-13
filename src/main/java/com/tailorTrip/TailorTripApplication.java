61f5b68cc5105370b04faea7b0a7a6c1e3d13f64
myo22 <dlalsgud12@naver.com>
2024-09-13 15:43:20 +0900

    기본적인 환경 설정

--- src/main/java/com/tailorTrip/TailorTripApplication.java
+++ src/main/java/com/tailorTrip/TailorTripApplication.java
@@ -1,4 +1,4 @@
-package com.tailortrip;
+package com.tailorTrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/tailorTrip/TailorTripApplicationTests.java
+++ src/test/java/com/tailorTrip/TailorTripApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tailortrip;
+package com.tailorTrip;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

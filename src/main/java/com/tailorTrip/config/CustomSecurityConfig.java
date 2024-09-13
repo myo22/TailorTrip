152efba18ace88a451c8892d2aa94228fbef43a8
myo22 <dlalsgud12@naver.com>
2024-09-13 16:56:18 +0900

    Spring Security 설정

---
+++ src/main/java/com/tailorTrip/config/CustomSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.tailorTrip.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Log4j2
+@RequiredArgsConstructor
+public class CustomSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        log.info("------------------configure----------------------");
+
+        http.formLogin();
+
+        return http.build();
+    }
+
+    // css 파일, js 파일 등 정적파일들에는 굳이 시큐리티를 적용할 필요가 없다.
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+
+        log.info("------------------web Configure-----------------------");
+
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+}

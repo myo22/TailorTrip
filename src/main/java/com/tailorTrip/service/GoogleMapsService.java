2877862e0b2da06766670801c05eec63b5935f15
myo22 <dlalsgud12@naver.com>
2024-09-18 14:45:38 +0900

    Google Directions API 연동

---
+++ src/main/java/com/tailorTrip/service/GoogleMapsService.java
@@ -0,0 +1,28 @@
+package com.tailorTrip.service;
+
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.TravelMode;
+import lombok.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GoogleMapsService {
+
+    private final GeoApiContext context;
+
+    public GoogleMapsService(@Value("${google.api.key}") String apiKey) {
+        this.context = new GeoApiContext.Builder()
+                .apiKey(apiKey)
+                .build();
+    }
+
+    public DirectionsResult getDirections(String origin, String destination) throws Exception {
+        return DirectionsApi.newRequest(context)
+                .mode(TravelMode.DRIVING) // 운전 모드, 필요에 따라 변경 가능
+                .origin(origin)
+                .destination(destination)
+                .await();
+    }
+}

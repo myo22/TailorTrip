8ca14f3ac3d44e5869615266126a03721f9f7189
myo22 <dlalsgud12@naver.com>
2024-09-18 15:58:20 +0900

    Google Places API 연동

---
+++ src/main/java/com/tailorTrip/controller/PlacesController.java
@@ -0,0 +1,24 @@
+package com.tailorTrip.controller;
+
+import com.google.maps.model.PlacesSearchResponse;
+import com.tailorTrip.service.GooglePlaceService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PlacesController {
+
+    private final GooglePlaceService googlePlaceService;
+
+    public PlacesController(GooglePlaceService googlePlaceService) {
+        this.googlePlaceService = googlePlaceService;
+    }
+
+    @GetMapping("/places")
+    public PlacesSearchResponse searchPlaces(
+            @RequestParam String query,
+            @RequestParam String location) throws Exception {
+        return googlePlaceService.searchPlaces(query, location);
+        }
+}

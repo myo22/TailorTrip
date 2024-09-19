06417e1222d9faecb8655ac931f880c7991a3eb1
myo22 <dlalsgud12@naver.com>
2024-09-19 18:27:32 +0900

    자동 로그인 처라

--- src/main/java/com/tailorTrip/config/CustomSecurityConfig.java
+++ src/main/java/com/tailorTrip/config/CustomSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.tailorTrip.config;
 
+import com.tailorTrip.security.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -11,6 +12,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @Log4j2
@@ -18,14 +23,25 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class CustomSecurityConfig {
 
+    //주입 필요
+    private final DataSource dataSource;
+    private final CustomUserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         log.info("------------------configure----------------------");
 
+        // 커스텀 로그인 페이지
         http.formLogin().loginPage("/member/login");
-
+        // CSRF 토큰 비활성화
         http.csrf().disable();
+
+        http.rememberMe()
+                .key("12345678")
+                .tokenRepository(persistentTokenRepository())
+                .userDetailsService(userDetailsService)
+                .tokenValiditySeconds(60*60*24*30);
 
         return http.build();
     }
@@ -45,6 +61,11 @@ public class CustomSecurityConfig {
         return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
-
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository(){
+        JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+        repo.setDataSource(dataSource);
+        return repo;
+    }
 
 }

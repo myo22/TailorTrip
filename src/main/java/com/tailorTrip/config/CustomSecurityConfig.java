3b72dc22f1b5e98019f495773b90ad63672996d6
myo22 <dlalsgud12@naver.com>
2024-09-20 15:38:03 +0900

    최신 스프링 시큐리티 보안 설정에 따른 람다식 변경

--- src/main/java/com/tailorTrip/config/CustomSecurityConfig.java
+++ src/main/java/com/tailorTrip/config/CustomSecurityConfig.java
@@ -28,28 +28,29 @@ public class CustomSecurityConfig {
     private final CustomUserDetailsService userDetailsService;
 
     @Bean
+    public PasswordEncoder passwordEncoder(){
+        // 종류중에서 가장 무난, 해시 알고리즘으로 암호화 처리
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         log.info("------------------configure----------------------");
 
         // 커스텀 로그인 페이지
-        http.formLogin().loginPage("/member/login");
+        http.formLogin(form -> form.loginPage("/member/login"));
         // CSRF 토큰 비활성화
-        http.csrf().disable();
+        http.csrf(csrf -> csrf.disable()
+        );
 
-        http.rememberMe()
+        http.rememberMe(rememberMe -> rememberMe
                 .key("12345678")
                 .tokenRepository(persistentTokenRepository())
                 .userDetailsService(userDetailsService)
-                .tokenValiditySeconds(60*60*24*30);
+                .tokenValiditySeconds(60*60*24*30));
 
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        // 종류중에서 가장 무난, 해시 알고리즘으로 암호화 처리
-        return new BCryptPasswordEncoder();
     }
 
     // css 파일, js 파일 등 정적파일들에는 굳이 시큐리티를 적용할 필요가 없다.

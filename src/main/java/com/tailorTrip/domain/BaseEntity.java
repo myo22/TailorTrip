47d2ea958b4e875982a3a3eaa9ddff6dccb84af1
myo22 <dlalsgud12@naver.com>
2024-09-20 17:08:11 +0900

    회원 데이터 만들기

--- src/main/java/com/tailorTrip/domain/BaseEntity.java
+++ src/main/java/com/tailorTrip/domain/BaseEntity.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @MappedSuperclass // 이 클래스를 상속받는 엔티티들이 해당 필드를 자동으로 상속받게 해줍니다. 데이터베이스 테이블로 매핑되지 않으며, 하위 클래스에서만 사용됩니다.
 @EntityListeners(value = {AuditingEntityListener.class}) // JPA의 감사(auditing) 기능을 활성화하여, 엔티티의 생성일(@CreatedDate)과 수정일(@LastModifiedDate)을 자동으로 관리합니다.
 @Getter
-public class BaseEntity {
+abstract class BaseEntity {
 
     @CreatedDate
     @Column(name = "regdate",updatable = false) // 필드가 엔티티 업데이트 시 수정되지 않음을 의미합니다.

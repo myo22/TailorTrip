2eec147da205a575da22f37e5aae870879be45e5
myo22 <dlalsgud12@naver.com>
2024-09-20 17:34:26 +0900

    스프링 시큐리티 회원 DTO를 설정하는법

--- src/main/java/com/tailorTrip/security/CustomUserDetailsService.java
+++ src/main/java/com/tailorTrip/security/CustomUserDetailsService.java
@@ -1,7 +1,11 @@
 package com.tailorTrip.security;
 
+import com.tailorTrip.Repository.MemberRepository;
+import com.tailorTrip.domain.Member;
+import com.tailorTrip.security.dto.MemberSecurityDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,27 +14,46 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
 
-    public CustomUserDetailsService() {
-        this.passwordEncoder = new BCryptPasswordEncoder();
-    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         log.info("loadUserByUsername: " + username);
 
-        UserDetails userDetails = User.builder()
-                .username("user1")
-                .password(passwordEncoder.encode("1111"))
-                .authorities("ROLE_USER")
-                .build();
+        Optional<Member> result = memberRepository.getWithRoles(username);
 
-        return userDetails;
+        if(result.isEmpty()) { // 해당 아이디를 가진 사용자가 없다면
+            throw new UsernameNotFoundException("username not found...");
+        }
+
+        Member member = result.get();
+
+        MemberSecurityDTO memberSecurityDTO =
+                new MemberSecurityDTO(
+                        member.getMid(),
+                        member.getMpw(),
+                        member.getEmail(),
+                        member.isDel(),
+                        false,
+                        member.getRoleSet()
+                                .stream().map(memberRole -> new SimpleGrantedAuthority("Role_" + memberRole.name()))
+                                .collect(Collectors.toList())
+                );
+
+        log.info("memberSecurityDTO");
+        log.info(memberSecurityDTO);
+
+
+        return memberSecurityDTO;
     }
 }

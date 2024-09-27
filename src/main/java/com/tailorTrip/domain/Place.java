ab8d4cbd62d109216cc7dd58da87bba927e2dd12
myo22 <dlalsgud12@naver.com>
2024-09-27 15:03:17 +0900

    장소 데이터 모델

--- src/main/java/com/tailorTrip/domain/Place.java
+++ src/main/java/com/tailorTrip/domain/Place.java
@@ -17,11 +17,19 @@ public class Place {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String placeId; // Google Places API의 place_id
     private String name; // 장소 이름
-    private String category; // 카테고리 (카페, 맛집, 관광명소)
+
     private double lat; // 위도
     private double lng; // 경도
 
-    private boolean walkable; // 도보 가능 여부
-    private boolean fastAccess; // 빠른 접근 가능 여부
+    private double raring; // 평점
+    private int userRatingTotal; // 총 평점 수
+
+    private String address; // 주소
+    private String phoneNumber; // 전화번호
+    private String website; // 웹 사이트
+
+    private String openingHours; // 운영 시간 (예: "09:00-18:00")
+    // 추가 필드 (예: 사진 URL 등)
 }

06e44a5e6ae9553c7336897ea2bfdb42c2554c13
myo22 <dlalsgud12@naver.com>
2024-10-10 17:23:31 +0900

    데이터베이스 수준에서 지역별 필터링을 수행하면, 메모리 사용량과 처리 시간을 줄일 수 있습니다.

--- src/main/java/com/tailorTrip/Repository/PlaceRepository.java
+++ src/main/java/com/tailorTrip/Repository/PlaceRepository.java
@@ -3,5 +3,9 @@ package com.tailorTrip.Repository;
 import com.tailorTrip.domain.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlaceRepository extends JpaRepository<Place, Long> {
+
+    List<Place> findByAddr1Containing(String region);
 }

ef6c4b5d170a252506ac8280a2fd0820d66f219d
myo22 <dlalsgud12@naver.com>
2024-10-26 18:26:37 +0900

    Refresh Token 구현과 예외 처리

--- src/main/java/com/tailorTrip/security/filter/RefreshTokenFilter.java
+++ src/main/java/com/tailorTrip/security/filter/RefreshTokenFilter.java
@@ -1,6 +1,10 @@
 package com.tailorTrip.security.filter;
 
+import com.google.gson.Gson;
+import com.tailorTrip.security.exception.RefreshTokenException;
 import com.tailorTrip.util.JWTUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,6 +14,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Map;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -26,11 +33,79 @@ public class RefreshTokenFilter extends OncePerRequestFilter {
 
         if (!path.equals(refreshPath)){
             log.info("skip refresh token filter......");
+            // 필터 체인을 계속 실행합니다. 다음 순서로 넘겨버림.
             filterChain.doFilter(request,response);
             return;
         }
 
         log.info("Refresh Token Filter...run............1");
+
+        //전송된 JSON에서 accessToken과 refreshToken을 얻어온다.
+        Map<String, String> tokens = parseRequestJSON(request);
+
+        String accessToken = tokens.get("accessToken");
+        String refreshToken = tokens.get("refreshToken");
+
+        log.info("accessToken: " + accessToken);
+        log.info("refreshToken: " + refreshToken);
+
+        try {
+            checkAccessToken(accessToken);
+        }catch (RefreshTokenException refreshTokenException){
+            refreshTokenException.sendResponseError(response);
+        }
+
+        Map<String, Object> refreshClaims = null;
+
+        try {
+
+            refreshClaims = checkRefreshToken(refreshToken);
+            log.info(refreshClaims);
+
+        }catch (RefreshTokenException refreshTokenException){
+            refreshTokenException.sendResponseError(response);
+            return; // 더 이상 실행할 코드가 없음
+        }
     }
 
+    private Map<String, String> parseRequestJSON(HttpServletRequest request){
+
+        //JSON 데이터를 분석해서 mid, mpw 전달 값을 Map으로 처리
+        try(Reader reader = new InputStreamReader(request.getInputStream())){
+
+            Gson gson = new Gson();
+            // JSON 데이터를 Java의 Map<String, String> 형태로 변환합니다.
+            return gson.fromJson(reader, Map.class);
+
+        }catch (Exception e){
+            log.error(e.getMessage());
+        }
+        return null;
+    }
+
+    private void checkAccessToken(String accessToken) throws RefreshTokenException {
+        try {
+            jwtUtil.validateToken(accessToken);
+        }catch (ExpiredJwtException exception){
+            log.info("Access Token has expired");
+        }catch (Exception exception){
+            throw new RefreshTokenException(RefreshTokenException.ErrorCase.NO_ACCESS);
+        }
+    }
+
+    private Map<String, Object> checkRefreshToken(String refreshToken) throws RefreshTokenException{
+
+        try {
+            Map<String, Object> values = jwtUtil.validateToken(refreshToken);
+            return values;
+        }catch(ExpiredJwtException expiredJwtException) {
+            throw new RefreshTokenException(RefreshTokenException.ErrorCase.OLD_REFRESH);
+        }catch (MalformedJwtException malformedJwtException){
+            log.error("MalformedJwtException------------------");
+            throw new RefreshTokenException(RefreshTokenException.ErrorCase.NO_REFRESH);
+        }catch (Exception exception){
+            new RefreshTokenException(RefreshTokenException.ErrorCase.NO_REFRESH);
+        }
+        return null;
+    }
 }

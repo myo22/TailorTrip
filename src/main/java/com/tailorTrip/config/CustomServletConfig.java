db74a36b7411d33e9228f963d0c16fa4e62972e2
myo22 <dlalsgud12@naver.com>
2024-10-26 17:07:50 +0900

    나중에 파라미터 타입의 변환을 추가할 수 있도록 생성, '/files/'로 시작하는 경로는 스프링 MVC에서 일반 파일 경로로 처리하도록 지정

--- src/main/java/com/tailorTrip/config/CustomServletConfig.java
+++ src/main/java/com/tailorTrip/config/CustomServletConfig.java
@@ -1,6 +1,15 @@
 package com.tailorTrip.config;
 
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class CustomServletConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/files/**")
+                .addResourceLocations("classpath:/static/");
+    }
 }

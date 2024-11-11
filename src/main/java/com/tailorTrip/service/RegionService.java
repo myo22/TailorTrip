f43c807d8ea80c5eb342a7be36d4a89edd8ac81a
myo22 <dlalsgud12@naver.com>
2024-11-11 10:34:29 +0900

    areaCode 변경

--- src/main/java/com/tailorTrip/service/RegionService.java
+++ src/main/java/com/tailorTrip/service/RegionService.java
@@ -19,12 +19,12 @@ public class RegionService
         REGION_COORDINATES.put("6", new double[]{35.1796, 129.0756});
         REGION_COORDINATES.put("7", new double[]{35.5396, 129.3114});
         REGION_COORDINATES.put("8", new double[]{36.4800, 127.2892});
-        REGION_COORDINATES.put("31", new double[]{37.4138, 127.5183});
+        REGION_COORDINATES.put("31", new double[]{37.2752, 127.0093});
         REGION_COORDINATES.put("32", new double[]{37.8228, 128.1555});
         REGION_COORDINATES.put("33", new double[]{36.6356, 127.4913});
         REGION_COORDINATES.put("34", new double[]{36.6355, 126.6520});
         REGION_COORDINATES.put("35", new double[]{36.0726, 128.6075});
-        REGION_COORDINATES.put("36", new double[]{35.1796, 128.0955});
+        REGION_COORDINATES.put("36", new double[]{35.2271, 128.6812});
         REGION_COORDINATES.put("37", new double[]{35.7193, 127.1574});
         REGION_COORDINATES.put("38", new double[]{34.8671, 126.9911});
         REGION_COORDINATES.put("39", new double[]{33.4996, 126.5312});

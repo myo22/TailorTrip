474d9962bcff91f9774ec6b1a1886bf0804c4a72
myo22 <dlalsgud12@naver.com>
2024-12-22 15:39:11 +0900

    CORS 문제를 해결하기 위한 웹 설정

---
+++ src/main/java/com/tailorTrip/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.tailorTrip.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://yourdomain.com")  // CORS 허용 도메인 설정
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}

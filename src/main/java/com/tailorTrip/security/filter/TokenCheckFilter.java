fad299aa5a9a6c8330a31e4208c9d692fd274bd8
myo22 <dlalsgud12@naver.com>
2025-01-22 17:33:06 +0900

    save 기능만 필터하게 변경

--- src/main/java/com/tailorTrip/security/filter/TokenCheckFilter.java
+++ src/main/java/com/tailorTrip/security/filter/TokenCheckFilter.java
@@ -32,7 +32,7 @@ public class TokenCheckFilter extends OncePerRequestFilter {
 
         String path = request.getRequestURI();
 
-        if(!path.startsWith("/api/")) { // api로 시작하는 모든 필터에 적용
+        if(!path.startsWith("/save")) { // save로 시작하는 모든 필터에 적용 즉, 토크없이 접근해서는 안 되는 기능에 제한을 건다.
             filterChain.doFilter(request, response);
             return;
         }

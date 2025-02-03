30b5cabb08f63ec08b825f4fb26dae92c05475c5
myo22 <dlalsgud12@naver.com>
2025-02-03 19:21:37 +0900

    사용자 정보를 정확하게 분리

--- src/main/java/com/tailorTrip/dto/UserProfileDTO.java
+++ src/main/java/com/tailorTrip/dto/UserProfileDTO.java
@@ -2,6 +2,7 @@ package com.tailorTrip.dto;
 
 import com.tailorTrip.domain.MemberRole;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,8 +12,10 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class UserProfileDTO {
-    public String username;
-    public String email;
-    private Set<MemberRole> roles;
+    private String username;  // 사용자 고유 ID
+    private String email; // 사용자 이메일
+    private boolean del; // 삭제된 사용자 여부
+    private boolean social; // 소셜 로그인 여부
 }

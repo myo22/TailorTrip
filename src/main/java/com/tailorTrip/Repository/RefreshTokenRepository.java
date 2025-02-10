86f3e2af4dcd4890bbc1d27af96c1731b1e547e3
myo22 <dlalsgud12@naver.com>
2025-02-10 18:47:23 +0900

    RefreshToken 관리를 위한 Repository

--- src/main/java/com/tailorTrip/Repository/RefreshTokenRepository.java
+++ src/main/java/com/tailorTrip/Repository/RefreshTokenRepository.java
@@ -3,7 +3,10 @@ package com.tailorTrip.Repository;
 import com.tailorTrip.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
+    Optional<RefreshToken> findByMid(String mid);
     void deleteByMid(String mid);
 }

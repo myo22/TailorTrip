8848319b5a0f45233ed50ebb141d4db2507ee62f
myo22 <dlalsgud12@naver.com>
2025-03-12 17:12:34 +0900

    로그인, 회원가입 페이지 추가

---
+++ src/main/resources/static/auth/register/js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+
+  const opaButton = document.querySelector('.opa')
+  const openButton = document.querySelector('.emailbutton')
+
+  openButton.addEventListener('click', function () {
+    opaButton.classList.toggle('active');
+
+
+
+
+  });
+
+
+
+});

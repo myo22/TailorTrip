49dfc2863d40923a1bf968a8435bca1ac2102de7
myo22 <dlalsgud12@naver.com>
2025-03-14 19:05:52 +0900

    트랜잭션 범위 밖에서 엔터티 조작 오류 해결

--- src/main/java/com/tailorTrip/Repository/RefreshTokenRepository.java
+++ src/main/java/com/tailorTrip/Repository/RefreshTokenRepository.java
@@ -2,12 +2,14 @@ package com.tailorTrip.Repository;
 
 import com.tailorTrip.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     Optional<RefreshToken> findByMid(String mid);
+    @Transactional
     void deleteByMid(String mid);
     void deleteByToken(String token);
 }
